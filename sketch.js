5ff7601174bcc5664b715fb6215658f0908d8d79
ItzScarlettrose <69930603+ItzScarlettrose@users.noreply.github.com>
2020-09-25 21:17:31 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var mr, fr
+
+function setup() {
+  createCanvas(800,400);
+ mr=createSprite(400, 200, 50, 50);
+fr=createSprite(200,200,80,30)
+}
+function draw() {
+  background(255,255,255);  
+  mr.x= World.mouseX
+  mr.y= World.mouseY
+  if(mr.x-fr.x<mr.width/2+fr.width/2&&
+  fr.x-mr.x<mr.width/2+fr.width/2&&
+mr.y-fr.y<mr.height/2+fr.height/2&&
+    fr.y-mr.y<mr.height/2+fr.height/2){
+    
+
+mr.shapeColor="red"
+fr.shapeColor="red"
+}
+else{
+  mr.shapeColor="blue"
+  fr.shapeColor="blue"
+  
+}
+  drawSprites();
+}
